3cf40eb1847013508694155db1d783d291516073
jordan.werre <47542013+jwerre1@users.noreply.github.com>
2019-03-26 21:51:52 -0400

    now possible for words to be more than 1 word or have symbols

--- Word.js
+++ Word.js
@@ -1,17 +1,32 @@
 var Letter = require("./Letter.js");
 
+// make "if else" so that if the character in a word is a space, it just pushes the space
 var Word = function() {
     this.letterArray = [];
     this.addLetter = function(input) {
         var splitWord = input.split("");
+        var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
         for (var h = 0; h < splitWord.length; h++) {
+            var n = alphabet.includes(splitWord[h])
+
+            if (n === true) {
             this.letterArray.push(new Letter(splitWord[h]));
+            }
+
+            else {
+                this.letterArray.push(splitWord[h]);
+            }
         }
     };
     this.display = function() {
         var displayWord = this.letterArray[0].dashes();
         for (var i = 1; i < this.letterArray.length; i++) {
+            if (typeof(this.letterArray[i]) === "object") {
             displayWord += " " + this.letterArray[i].dashes();
+            }
+            else {
+                displayWord += " " + this.letterArray[i];
+            }
         }
         return displayWord;
     };
@@ -23,7 +38,7 @@ var Word = function() {
 }
 
 var test = new Word();
-test.addLetter("JORDAN");
+test.addLetter("STAR WARS IV : A NEW HOPE");
 console.log(test.letterArray);
 console.log(test.display());
 
@@ -42,3 +57,5 @@ console.log(test.display());
 
 // genWordArray(wordOne);
 // console.log(wordArray);
+
+module.exports = Word;
---
+++ index.js
@@ -0,0 +1,3 @@
+var Word = require("./Word");
+
+FIREMAN 
